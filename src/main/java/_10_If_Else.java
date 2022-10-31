a46da8c0a48da91347e4da01d272bc1b5d176717
Sezgin VARLI <varlisezginn@gmail.com>
2022-10-31 12:18:55 +0300

    update

---
+++ src/main/java/_10_If_Else.java
@@ -0,0 +1,12 @@
+public class _10_If_Else {
+    public static void main(String[] args) {
+        boolean learning = true;
+
+        if (learning){
+            System.out.println("Java Developer");
+        }
+        else{
+            System.out.println("Learn Java please");
+        }
+    }
+}
