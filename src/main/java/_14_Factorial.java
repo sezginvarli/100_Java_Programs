be209f6f9cda897593470e069b7c2e511c8c2fed
Sezgin VARLI <varlisezginn@gmail.com>
2022-11-09 10:59:32 +0300

    update

---
+++ src/main/java/_14_Factorial.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class _14_Factorial {
+    public static void main(String[] args) {
+        int n,c, fact=1;
+
+        System.out.println("Enter an integer to calculate it's factorial");
+        Scanner in= new Scanner(System.in);
+
+        n= in.nextInt();
+
+        if (n<0)
+            System.out.println("Number should be non-negative");
+        else{
+            for (c=1; c<=n;c++)
+                fact=fact*c;
+            System.out.println("factorial of "+n+" is = " +fact);
+        }
+    }
+}
