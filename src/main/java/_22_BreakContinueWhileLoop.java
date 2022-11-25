5b6fb031c7dde0cf7488f8a2c5fb827b8306c52a
Sezgin VARLI <varlisezginn@gmail.com>
2022-11-25 14:36:18 +0300

    update

---
+++ src/main/java/_22_BreakContinueWhileLoop.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class _22_BreakContinueWhileLoop {
+    public static void main(String[] args) {
+        int n;
+
+        Scanner input = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Input an Integer");
+            n = input.nextInt();
+
+            if (n != 0) {
+                System.out.println("You entered " + n);
+                continue;
+            } else {
+                break;
+            }
+
+        }
+
+    }
+}
