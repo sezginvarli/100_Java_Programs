131ba7c8703874c15b7f24f7e93a6355cf2ba0b3
Sezgin VARLI <varlisezginn@gmail.com>
2022-11-25 14:38:44 +0300

    update

---
+++ src/main/java/_23_Alphabets.java
@@ -0,0 +1,10 @@
+public class _23_Alphabets {
+    public static void main(String[] args) {
+        char ch;
+
+        for (ch = 'a'; ch <= 'z'; ch++) {
+            System.out.print(ch);
+
+        }
+    }
+}
