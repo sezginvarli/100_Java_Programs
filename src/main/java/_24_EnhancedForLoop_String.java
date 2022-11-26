9b132afe0297f064a5f1c65154de6a40a19d57ae
Sezgin VARLI <varlisezginn@gmail.com>
2022-11-26 18:47:09 +0300

    update

---
+++ src/main/java/_24_EnhancedForLoop_String.java
@@ -0,0 +1,9 @@
+public class _24_EnhancedForLoop_String {
+    public static void main(String[] args) {
+        String languages [] = {"C","C++","Java","Python","Ruby"};
+
+        for (String sample : languages){
+            System.out.println(sample);
+        }
+    }
+}
---
+++ src/main/java/_25_MultiplicationTable.java
@@ -0,0 +1,2 @@
+public class _25_MultiplicationTable {
+}
